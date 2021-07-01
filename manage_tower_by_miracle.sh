32f5518adedc2bb73b098714eb677c2699f1eda2
Jakob Holst <jho@miracle.dk>
2021-07-01 09:22:03 +0200

    focus on venv

--- manage_tower_by_miracle.sh
+++ manage_tower_by_miracle.sh
@@ -19,16 +19,24 @@ tower-cli role           grant  --type use --user miracle  --project awx-turnkey
 
 tower-cli inventory      create  --name awx-turnkey --organization miracle --variables @awx-turnkey.json
 tower-cli host           create  --name centos8venv --description "the host for awx by miracle and virtual environments" -i awx-turnkey --enabled True 
+tower-cli group          create --name awx-turnkey-venv --description "Server to be deployed with awx on virtual environment" --inventory awx-turnkey
+
+tower-cli host           associate  --host centos8venv --group awx-turnkey-venv
 tower-cli credential     create  --name maas        --description "access to maas servers" --credential-type Machine --organization miracle  --inputs="{\"username\":\"cloud-user\",\"ssh_key_data\":\"$(sed -E ':a;N;$!ba;s/\r{0,1}\n/\\n/g' ~/.ssh/id_rsa)\n\",\"become_method\":\"sudo\"}"
 
-tower-cli job_template   create  --name "ansible_fact_collecter"              --description "Collect ansible facts"                   --inventory awx-turnkey --project awx-turnkey --playbook "ansiblefacts.yml"                        --credential maas
-tower-cli job_template   create  --name "awx-turnkey-create-project-servers"  --description "Terraform server on maas"                --inventory awx-turnkey --project awx-turnkey --playbook "awx-turnkey-create-project-servers.yml"  --credential maas
-tower-cli job_template   create  --name "awx-turnkey-on-kubernetes"           --description "autodeploy awx on kubernetes"            --inventory awx-turnkey --project awx-turnkey --playbook "awx-turnkey-on-kubernetes.yml"           --credential maas
+#tower-cli job_template   create  --name "ansible_fact_collecter"              --description "Collect ansible facts"                   --inventory awx-turnkey --project awx-turnkey --playbook "ansiblefacts.yml"                        --credential maas
+#tower-cli job_template   create  --name "awx-turnkey-create-project-servers"  --description "Terraform server on maas"                --inventory awx-turnkey --project awx-turnkey --playbook "awx-turnkey-create-project-servers.yml"  --credential maas
+#tower-cli job_template   create  --name "awx-turnkey-on-kubernetes"           --description "autodeploy awx on kubernetes"            --inventory awx-turnkey --project awx-turnkey --playbook "awx-turnkey-on-kubernetes.yml"           --credential maas
 tower-cli job_template   create  --name "awx-turnkey-on-venv"                 --description "autodeploy awx on virtual environment"   --inventory awx-turnkey --project awx-turnkey --playbook "awx-turnkey-on-venv.yml"                 --credential maas
-tower-cli job_template   create  --name "awx-turnkey-on-scl"                  --description "autodeploy awx on software collections"  --inventory awx-turnkey --project awx-turnkey --playbook "awx-turnkey-on-scl.yml"                 --credential maas
+#tower-cli job_template   create  --name "awx-turnkey-on-scl"                  --description "autodeploy awx on software collections"  --inventory awx-turnkey --project awx-turnkey --playbook "awx-turnkey-on-scl.yml"                 --credential maas
 
 
-tower-cli workflow       create   --name "awx-turnkey" --description "Awx turnkey workflow"  -e @awx-turnkey.json --organization miracle 
+#tower-cli workflow       create   --name "awx-turnkey" --description "Awx turnkey workflow"  -e @awx-turnkey.json --organization miracle 
+
+#tower-cli  workflow schema  awx-turnkey @awx-turnkey-workflow.yaml
+
+#tower-cli  schedule create -n awx-turnkey-schedule --workflow awx-turnkey --enabled True --rrule "DTSTART;TZID=UTC:20210630T093758 RRULE:FREQ=MINUTELY;INTERVAL=5"
+
 
 
 
